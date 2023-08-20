e2a2571564a0adbcfb41ed105443689797e7a026
Tanmayjain0906 <tanmayjain983@gmail.com>
2023-08-20 13:59:06 +0530

    sfds

--- script.js
+++ script.js
@@ -1,63 +1,61 @@
 
 let button = document.getElementsByClassName("student-detail")[0];
-let addButton = document.getElementById("editBtn");
+let editButton = document.getElementById("editBtn");
 let tbody = document.getElementsByClassName("my-tbody")[0];
 let idNo = 1;
 button.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    
-   if(addButton.style.display === "")
-   {
-    console.log("1");
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let gpa = document.getElementById("gpa").value;
-    let age = document.getElementById("age").value;
-    let degree = document.getElementById("degree").value;
 
-    let tr = document.createElement("tr");
-    tr.className = `${idNo}`
-    let td1 = document.createElement("td");
-    let td2 = document.createElement("td");
-    let td3 = document.createElement("td");
-    let td4 = document.createElement("td");
-    let td5 = document.createElement("td");
-    let td6 = document.createElement("td");
+    if (editButton.style.display === "") {
+        
+        let name = document.getElementById("name").value;
+        let email = document.getElementById("email").value;
+        let gpa = document.getElementById("gpa").value;
+        let age = document.getElementById("age").value;
+        let degree = document.getElementById("degree").value;
 
-    td1.innerText = `${idNo}`;
-    idNo++;
+        let tr = document.createElement("tr");
+        tr.className = `${idNo}`
+        let td1 = document.createElement("td");
+        let td2 = document.createElement("td");
+        let td3 = document.createElement("td");
+        let td4 = document.createElement("td");
+        let td5 = document.createElement("td");
+        let td6 = document.createElement("td");
 
-    td2.innerText = name;
+        td1.innerText = `${idNo}`;
+        idNo++;
 
-    td3.innerText = email;
+        td2.innerText = name;
 
-    td4.innerText = age;
+        td3.innerText = email;
 
-    td5.innerText = gpa;
+        td4.innerText = age;
 
-    td6.innerHTML = `<div>${degree}</div> <div class="edit-buttons"><button class="edit"  onclick="editBtn(${idNo})"><i class="fa-solid fa-pen-to-square" style="color: #0c4cbb;"></i></button><button class="delete"  onclick="deleteBtn(${idNo})"><i class="fa-solid fa-trash" style="color: #0c4cbb;"></i></button></div>`;
+        td5.innerText = gpa;
 
-    tr.append(td1, td2, td3, td4, td5, td6);
-    tbody.appendChild(tr);
+        td6.innerHTML = `<div>${degree}</div> <div class="edit-buttons"><button class="edit"  onclick="editBtn(${idNo})"><i class="fa-solid fa-pen-to-square" style="color: #0c4cbb;"></i></button><button class="delete"  onclick="deleteBtn(${idNo})"><i class="fa-solid fa-trash" style="color: #0c4cbb;"></i></button></div>`;
 
-    document.getElementById("name").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("gpa").value = "";
-    document.getElementById("age").value = "";
-    document.getElementById("degree").value = "";
-   }
-   else
-   {
-    addButton.style.display = "";
-    document.getElementById("name").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("gpa").value = "";
-    document.getElementById("age").value = "";
-    document.getElementById("degree").value = "";
-   }
-   
-    
+        tr.append(td1, td2, td3, td4, td5, td6);
+        tbody.appendChild(tr);
+
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("gpa").value = "";
+        document.getElementById("age").value = "";
+        document.getElementById("degree").value = "";
+    }
+    else {
+        editButton.style.display = "";
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("gpa").value = "";
+        document.getElementById("age").value = "";
+        document.getElementById("degree").value = "";
+    }
+
+
 })
 
 
@@ -82,9 +80,11 @@ function search() {
 
         if ((tdNameText.indexOf(searchInput) > -1) || (tdEmailText.indexOf(searchInput) > -1) || (tdDegreeText.indexOf(searchInput) > -1)) {
             tr[i].style.display = "";
+            
         }
         else {
             tr[i].style.display = "none";
+            
         }
     }
 }
@@ -92,29 +92,36 @@ function search() {
 
 function deleteBtn(data) {
 
-    let removeId = (data - 1).toString();
-    let newId = data - 1;
-    let index = -1;
+    let a = confirm("Are you sure");
 
-    let tr = tbody.getElementsByTagName("tr");
+    if (a) {
 
-    for (let i = 0; i < tr.length; i++) {
-        let trId = tr[i].className;
+        let removeId = (data - 1).toString();
+        
+        
 
-        if (removeId === trId) {
-            tbody.removeChild(tr[i]);
-            index = i;
+        let tr = tbody.getElementsByTagName("tr");
+
+        for (let i = 0; i < tr.length; i++) {
+            let trId = tr[i].className;
+
+            if (removeId === trId) {
+                tbody.removeChild(tr[i]);
+                
+            }
         }
     }
+
+
 
 }
 
 
 function editBtn(data) {
     let changeId = (data - 1).toString();
-
-    addButton.style.display = "block";
     
+    editButton.style.display = "block";
+
     let index = -1;
     let tr = tbody.getElementsByTagName("tr");
 
@@ -131,13 +138,18 @@ function editBtn(data) {
         }
     }
 
-    addButton.addEventListener("click", () => {
-        console.log("2");
-        tr[index].getElementsByTagName("td")[1].innerText = document.getElementById("name").value;
-        tr[index].getElementsByTagName("td")[2].innerText = document.getElementById("email").value;
-        tr[index].getElementsByTagName("td")[3].innerText = document.getElementById("age").value;
-        tr[index].getElementsByTagName("td")[4].innerText = document.getElementById("gpa").value;
-        tr[index].getElementsByTagName("td")[5].getElementsByTagName("div")[0].innerText = document.getElementById("degree").value;
+    editButton.addEventListener("click", () => {
+        
+        if(index > -1)
+        {
+            tr[index].getElementsByTagName("td")[1].innerText = document.getElementById("name").value;
+            tr[index].getElementsByTagName("td")[2].innerText = document.getElementById("email").value;
+            tr[index].getElementsByTagName("td")[3].innerText = document.getElementById("age").value;
+            tr[index].getElementsByTagName("td")[4].innerText = document.getElementById("gpa").value;
+            tr[index].getElementsByTagName("td")[5].getElementsByTagName("div")[0].innerText = document.getElementById("degree").value;
+        }
+        
+        index = -1;
     })
-    
+
 }
